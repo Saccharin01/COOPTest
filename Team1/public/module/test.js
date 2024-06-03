f4a85648dd0b47d90343026dd51ec275f1218308
KimInteger <kjs20151240@gmail.com>
2024-06-03 12:55:21 +0900

    test를 위한 js파일 생성

---
+++ Team1/public/module/test.js
@@ -0,0 +1,12 @@
+// * 해당 파일은 모듈을 만들기 전 테스트로 만들어보는 파일입니다.
+// * 객체에 함수를 담아서 접근할 수 있는지 확인합니다.
+
+const calculate = {
+  plus : function(a,b){
+    return a + b;
+  },
+
+  minus : function(a,b){
+    return a - b;
+  }
+};
