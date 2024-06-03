637ab625be6ed1a51b28fa5699328e9b21493185
유으뜸 <www.goolge.com>
2024-06-03 18:05:08 +0900

    추가 설명 작성

--- Team1/public/index.js
+++ Team1/public/index.js
@@ -1,6 +1,10 @@
+// 버튼 태그에 id를 btn이라고 설정하고 버튼을 불러옴
 const button = document.getElementById("btn");
 // console.log(button);
 
+// 불러온 버튼에 클릭 이벤트를 부여했지만, 따로 핸들러 함수를 설정하지 않고
+// 다이렉트로 바로 적용했음
+// 원하시면 핸들러 함수 따로 설정하겠음
 button.addEventListener("click", () => {
   // body태그안에 id 이름이 user_id인 value값 가져오고 선언된 userID에 할당
   let userID = document.getElementById("user_id").value;
