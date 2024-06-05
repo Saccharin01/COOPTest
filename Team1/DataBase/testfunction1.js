d4e9cb7b07c0f1bee882fb9fd21214bcbcf0ae80
jojayeon <jojayeon6152@gmail.com>
2024-06-05 16:40:26 +0900

    테스트용 생성자 함수 생성

--- Team1/DataBase/testfunction1.js
+++ Team1/DataBase/testfunction1.js
@@ -1,1 +1,16 @@
-console.log("테스트 함수용 1번 ")
+//import테스트를 위한 export 함수만들기
+
+
+export class test1 {
+  constructor(a,b) {
+    this.a = a,
+    this.b = b
+  }
+  set(value){
+    if(value === "number"){
+      console.log("숫자 말고 다른 것 넣어주세요");
+    }
+  }
+}
+
+
