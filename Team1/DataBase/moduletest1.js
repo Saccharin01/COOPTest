e78e6746a9c11b8dacf709e4f88dd1ee7b1b62b5
jojayeon <jojayeon6152@gmail.com>
2024-06-05 16:10:24 +0900

    module Test: appearBtn 2번은 실행안됨 그래서 원인 파악

--- Team1/DataBase/moduletest1.js
+++ Team1/DataBase/moduletest1.js
@@ -3,3 +3,7 @@ import {makeObject} from "../public/module/module_createObject.js"
 // 우선 최소한 내가 아는 콘솔 표시해서 나오는지 테스트
 let a = new makeObject("ㅁㄴㅇㄴㅁㅇ","ㄴㅁㅇㄴㅁ");
 console.log(a);
+
+import {appearBtn} from "../public/module/module_appearSendBtn.js"
+let b = appearBtn("a","s","d");
+console.log(b);
