3df16bb19c2c708e448872966868c633c0ba4fee
Saccharin01 <chousik01@gmail.com>
2024-06-07 00:40:48 +0900

    testField done plz Check

--- Team1/public/test.js
+++ Team1/public/test.js
@@ -1,3 +1,12 @@
-import { makeObject as testing }  from "./module/module_createObject.js";
+import { appearBtn as testing }  from "./module/module_appearSendBtn.js";
 
 console.log(testing)
+console.dir(testing)
+
+const input1 =  document.getElementById('user_id')
+const input2 =  document.getElementById('user_pw')
+const btn =  document.getElementById('btn')
+
+console.log(input1, input2,btn)
+
+testing(input1,input2,btn)
