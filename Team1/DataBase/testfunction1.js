dd47f8d6efaaa6793abcdb36260555148ce65485
Saccharin01 <chousik01@gmail.com>
2024-06-06 21:51:47 +0900

    testing chief logic

--- Team1/DataBase/testfunction1.js
+++ Team1/DataBase/testfunction1.js
@@ -1,21 +1,28 @@
 //import테스트를 위한 export 함수만들기
+const func = {
+  
+  test1 : function(){
 
-
-export class test1 {
-  constructor(a,b) {
-    this.a = a,
-    this.b = b
-  }
-  set a(value){
-    if(typeof(value) === "number"){
-      this._a = value;
-    }else{
-      console.log("숫자넣어줘");
+    class test1 {
+      constructor(a,b) {
+        this.a = a,
+        this.b = b
+      }
+      set a(value){
+        if(typeof(value) === "number"){
+          this._a = value;
+        }else{
+          console.log("숫자넣어줘");
+        }
+      }
     }
+  },
+
+  test2 : 
+    function test2(a,b) {
+     return a*b;
   }
+
+
+
 }
-
-export function test2(a,b) {
-  return a*b;
-};
-
