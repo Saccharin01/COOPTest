92e6294c57e45f7ceaf056c68cafc277e12e5b2a
Saccharin01 <chousik01@gmail.com>
2024-06-06 23:53:32 +0900

    location reser

--- Team1/DataBase/deprecated/moduletest1.js
+++ Team1/DataBase/deprecated/moduletest1.js
@@ -1,5 +1,5 @@
 //import로 public/module/module_createObject.js 를 가져오자
-import {makeObject} from "../public/module/module_createObject.js"
+import {makeObject} from "../../public/module/module_createObject.js"
 // 우선 최소한 내가 아는 콘솔 표시해서 나오는지 테스트
 let a = new makeObject("ㅁㄴㅇㄴㅁㅇ","ㄴㅁㅇㄴㅁ");
 console.log(a);
