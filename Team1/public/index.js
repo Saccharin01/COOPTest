363794d12ab3b3497dff418cfe439893b9826e02
yooeuddeum <145522980+yooeuddeum@users.noreply.github.com>
2024-06-07 01:39:22 +0900

    이슈 건 수정

--- Team1/public/index.js
+++ Team1/public/index.js
@@ -1,39 +1,37 @@
-// * 사용자 데이터를 저장할 빈 배열
-let user = [];
+// * 1. 임의 변수 a에 빈 배열을 할당
+let a = [];
 
-// 버튼 태그 불러옴
-const button = document.getElementById("btn");
-console.log(button);
+// * 2. 빈 배열 속에 DOM API를 통해서 input 태그의 갯수 만큼 false 값을 적용
 
-// * 버튼 요소에 클릭 이벤트 부여하고 이벤트가 발생했을때 실행해서
-// * 단발성 기능 수행 / 아이디와 비밀번호가 빈 문자열이 아니라면 실행이 되게끔 수정
-// * 데이터 값이 추가되면 입력창에서 값을 지움
-// * 만약 input창에 아무것도 입력안하고 버튼 클릭시 에러가 뜨게 했음
-// * 마지막 초기화의 기준은 브라우저상에서 새로고침을 하거나 창을 닫을시 초기화 그렇지 않다면 데이터는 저장 된 상태에서 계속 유지(초기화 된 빈 배열 user)
-// * 보안적인 부분에서는 취약한 단점 또한 이 자쓰 코드는 폼 태그 기준에서 서버로 데이터를 보내지 않는 방식임
-// * 그래서 해당 방식은 버튼 type이 button일때 자쓰 자체에서 기능 구현으로 사용 용도
-// * 서버로 데이터를 보내는 형식의 코드는 버튼의 type을 submit으로 변경 하고 html상 전체 부모 기준으로
-// * 잡혀있는 form 태그 자체에 이벤트 리스너를 걸어주고 이벤트 자체를 submit을 적용한다.
-// * 그 이후 action 속성에 /submit으로 지정해주고 method 속성에 POST 방식으로 지정해준 후 endpoint를
-// * /submit으로 설정해주고 나머지는 chunk 단위로 데이터를 받아오고 JSON 구문을 파악한 후 파싱하든 모듈을 불러와서 qs 방식으로 하든 그건 취향껏 결정하면 되는 부분인것 같습니다. 
-button.addEventListener("click", () => {
-  const userIDInput = document.getElementById("user_id");
-  const userPWInput = document.getElementById("user_pw");
-  const userID = userIDInput.value;
-  const userPW = userPWInput.value;
+const inputs = document.querySelectorAll("input");
+// console.log(inputs);
+for (let i = 0; i < inputs.length; i++) {
+  a.push(false);
+}
+console.log("false 값이 추가 된 배열", a);
 
-  if (userID !== "" && userPW !== "") {
+// * 3. DOM API를 이용해서 유저가 입력한 정보를 조회
 
-    user.push({ userID, userPW });
-    console.log("회원가입이 가능한 아이디와 비밀번호입니다.");
-    console.log("현재 사용자 데이터:", user);
+const form = document.querySelector("form");
+// console.log(form)
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // 폼 제출 방지
 
-    userIDInput.value = "";
-    userPWInput.value = "";
-  } 
-  else {
-    console.error("아이디나 비밀번호를 입력해주세요.");
+  for (let i = 0; i < inputs.length; i++) {
+    const input = inputs[i];
+    const value = input.value;
+    console.log(`${input.id}:`, value);
+
+// * 4. 조회 된 정보의 데이터 타입이 공백 처리가 되어 있지 않은 문자열인지까지 확인
+
+    if (typeof value === "string" && value.trim() !== "") {
+      console.log(`입력 값 ${i}`);
+      a[i] = true; // * 5.데이터 타입이 문자열이라면 false 값 중 하나를 true로 변경
+    } else {
+      console.error(`입력 값이 없어 입력해`);
+    }
+    input.value = ""; // 입력 값 리셋
   }
-});
 
-console.log("초기화된 데이터", user);
+  console.log("form submit", a); // 배열 a의 상태를 최종적 확인
+});
