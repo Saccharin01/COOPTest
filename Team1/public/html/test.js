48d2af63a05e633711cdee2e82f211ce916374eb
KimInteger <kjs20151240@gmail.com>
2024-06-07 18:18:57 +0900

    test결과 정상 작동이 되나 문제점 발견

--- Team1/public/html/test.js
+++ Team1/public/html/test.js
@@ -1,1 +1,12 @@
-console.log('test용 js생성');
+import { appearBtn } from "../module/module_appearSendBtn.js"
+
+
+const input1 = document.getElementById('user_id')
+const input2 = document.getElementById('user_pw')
+const btn = document.getElementById('btn')
+
+console.log(input1);
+console.log(input2);
+
+input1.addEventListener('change',function(){appearBtn(input1,input2,btn)});
+input2.addEventListener('change',function(){appearBtn(input1,input2,btn)});
