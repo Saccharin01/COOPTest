43de0d40cd1dbcf3ff0941ec472dde841abf9786
jojayeon <jojayeon6152@gmail.com>
2024-06-08 17:48:48 +0900

    기본적인 submit 눌렸을 때 저장되게 제작

--- Team1/DataBase/DB.js
+++ Team1/DataBase/DB.js
@@ -6,3 +6,42 @@
 
 const http = require("http")
 const fs = require("fs")
+
+const server = http.createServer((req, res) => {
+  if(req.method === "POST"){
+    if(req.url === "/submit"){
+      let body = '';
+      req.on("data", chunk => {
+        body += chunk.toString();
+        });
+      req.on("end", () => {
+        const parseData = new URLSearchParams(body);
+        const id = parseData.get("uesr_id")
+        const pw = parseData.get("uesr_pw")
+
+        const json = {
+          id : id,
+          pw : pw
+        };
+
+        const jsondata = JSON.stringify(json);
+        fs.writeFile("database.json",jsondata, (err)=>{
+          if (err) {
+            res.end('오류');
+          }
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          res.end(jsondata);
+        })
+      })
+
+    }
+  }
+});
+
+const port = 8081;
+server.listen(port, (err)=>{
+  if(err) {
+    console.error("서버 리스너 에러");
+  }
+  console.log(`http://localhost:${port}`);
+});
