6c980c9d63fc5b6d5f750d33b3ce3f5ca9abe247
Saccharin01 <chousik01@gmail.com>
2024-06-09 21:58:00 +0900

    testField divide

---
+++ Team1/testapp.js
@@ -0,0 +1,34 @@
+const http = require(`http`);
+const XMLHttpRequest = require(`xhr2`);
+const xml = new XMLHttpRequest();
+const fs = require(`fs`)
+let container = [];
+
+let server = http.createServer((req, res)=>{
+  container.push(req.url)
+  console.log(container)
+  let index = container.indexOf(req.url)
+  console.log(index)
+  if(req.method === 'GET'){
+    container.push(req.url)
+    if(req.url==='/'){
+      fs.readFile('./team1/public/html/index.html', 'utf-8', (err,data)=>{
+        if(err){
+          console.log(`err occur : ${err}`)
+        }else{
+          res.writeHead(200, {'content-Type': 'text.html'});
+          res.write(data);
+          res.end();
+        }
+      })
+    }else if(container.includes(req.url)){
+      console.log('this part tracking')
+      console.log(container)
+    }
+  }
+})
+ 
+
+let PORT = 8080
+server.listen(`${PORT}`)
+console.log(`server running on: http://localhost:${PORT}/`)
