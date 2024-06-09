13bb944d09235dd6797efa3949b9a8313020c33d
Saccharin01 <chousik01@gmail.com>
2024-06-09 21:46:05 +0900

    testing dynamic url handling

--- Team1/app.js
+++ Team1/app.js
@@ -2,24 +2,24 @@ const http = require(`http`);
 const XMLHttpRequest = require(`xhr2`);
 const xml = new XMLHttpRequest();
 const fs = require(`fs`)
-
+let container = [];
 
 let server = http.createServer((req, res)=>{
   if(req.method === 'GET'){
-    console.log(req.method)
-    console.log(req.url)
+    container.push(req.url)
     if(req.url==='/'){
       fs.readFile('./team1/public/html/index.html', 'utf-8', (err,data)=>{
         if(err){
           console.log(`err occur : ${err}`)
         }else{
-          console.log(data)
           res.writeHead(200, {'content-Type': 'text.html'});
           res.write(data);
           res.end();
-          
         }
       })
+    }else if(container.includes(req.url)){
+      console.log('this part tracking')
+      console.log(container)
     }
   }
 })
