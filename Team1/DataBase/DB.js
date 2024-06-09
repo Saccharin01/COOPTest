239971fa9257cd7318b522fea1f61ca1801ba8de
jojayeon <jojayeon6152@gmail.com>
2024-06-09 15:06:53 +0900

    오타수정

--- Team1/DataBase/DB.js
+++ Team1/DataBase/DB.js
@@ -10,20 +10,27 @@ const fs = require("fs")
 const server = http.createServer((req, res) => {
   if(req.method === "POST"){
     if(req.url === "/submit"){
+      fs.readFile('./public/html/index.html', 'utf-8', (err, data)=>{
+        if(err){
+          console.log("errr");
+        }else{
+          res.writeHead(200, {'content-Type': 'text/html'})
+          res.write(data)
+        }
+      });
       let body = '';
       req.on("data", chunk => {
         body += chunk.toString();
         });
       req.on("end", () => {
         const parseData = new URLSearchParams(body);
-        const id = parseData.get("uesr_id")
-        const pw = parseData.get("uesr_pw")
+        const id = parseData.get("user_id")
+        const pw = parseData.get("user_pw")
 
         const json = {
           id : id,
           pw : pw
         };
-
         const jsondata = JSON.stringify(json);
         fs.writeFile("database.json",jsondata, (err)=>{
           if (err) {
