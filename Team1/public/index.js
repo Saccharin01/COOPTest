ba3ed2e615d9d0cb0a7178a23b0b8b0b723151c3
yooeuddeum <145522980+yooeuddeum@users.noreply.github.com>
2024-06-09 12:26:47 +0900

    마크업쪽 의견 수령

--- Team1/public/index.js
+++ Team1/public/index.js
@@ -1,37 +1,21 @@
-// * 1. 임의 변수 a에 빈 배열을 할당
-let a = [];
+// 각 요소 가져오기
+const userId = document.getElementById("user_id");
+const userPw = document.getElementById("user_pw");
+const userPwCheck = document.getElementById("user_pw-check");
+const submitBtn = document.getElementById("btn");
+submitBtn.style.display = "none";
+const root = document.getElementById("root");
+console.log(root);
 
-// * 2. 빈 배열 속에 DOM API를 통해서 input 태그의 갯수 만큼 false 값을 적용
-
-const inputs = document.querySelectorAll("input");
-// console.log(inputs);
-for (let i = 0; i < inputs.length; i++) {
-  a.push(false);
-}
-console.log("false 값이 추가 된 배열", a);
-
-// * 3. DOM API를 이용해서 유저가 입력한 정보를 조회
-
-const form = document.querySelector("form");
-// console.log(form)
-form.addEventListener("submit", function (e) {
-  e.preventDefault(); // 폼 제출 방지
-
-  for (let i = 0; i < inputs.length; i++) {
-    const input = inputs[i];
-    const value = input.value;
-    console.log(`${input.id}:`, value);
-
-// * 4. 조회 된 정보의 데이터 타입이 공백 처리가 되어 있지 않은 문자열인지까지 확인
-
-    if (typeof value === "string" && value.trim() !== "") {
-      console.log(`입력 값 ${i}`);
-      a[i] = true; // * 5.데이터 타입이 문자열이라면 false 값 중 하나를 true로 변경
-    } else {
-      console.error(`입력 값이 없어 입력해`);
-    }
-    input.value = ""; // 입력 값 리셋
+// 입력 값이 변경될 때마다 실행될 핸들러
+function checkInputs() {
+  if (userId.value && userPw.value && userPwCheck.value) {
+    submitBtn.style.display = "block";
+  } else {
+    submitBtn.style.display = "none";
   }
+}
 
-  console.log("form submit", a); // 배열 a의 상태를 최종적 확인
-});
+userId.addEventListener("input", checkInputs);
+userPw.addEventListener("input", checkInputs);
+userPwCheck.addEventListener("input", checkInputs);
