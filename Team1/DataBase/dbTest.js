04b314720e6547357b7bfa00a916ee9d32d9b759
Saccharin01 <chousik01@gmail.com>
2024-06-09 23:32:34 +0900

    copy req.res on app.js

--- Team1/DataBase/dbTest.js
+++ Team1/DataBase/dbTest.js
@@ -49,6 +49,41 @@ const server = http.createServer((req, res) => {
 
     }
   }
+  if(req.method === 'GET'){
+    if(req.url==='/style.css'){
+      // console.log('tracking')
+      fs.readFile('../public/html/style.css', (err,data)=>{
+        if(err){
+          console.log(`err occur : ${err}`)
+        }else{
+          // console.log(data)
+          res.writeHead(200, {'content-Type': 'text/css'})
+          res.write(data)
+          res.end()
+        }
+      })
+    }else if(req.url === '/index.js'){
+      fs.readFile('../public/index.js', 'utf-8', (err,data)=>{
+        if(err){
+          console.log(`err occur : ${err}`)
+        }else{
+          res.writeHead(200, {'content-Type':'Application/javaScript'})
+          res.write(data)
+          res.end()
+        }
+      })
+    }else if(req.url === '/favicon.ico'){
+      fs.readFile('../public/html/favicon.ico', (err,data)=>{
+        if(err){
+          console.log(`error occur : ${err}`)
+        }else {
+          res.writeHead(200, {'content-Type': 'image/ico'})
+          res.write(data)
+          res.end()
+        }
+      })
+    }
+  }
 });
 
 const port = 8081;
