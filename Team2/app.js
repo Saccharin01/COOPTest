1c083eb4b1d6eae142cf10ec029b6fe437505325
유으뜸 <www.goolge.com>
2024-06-12 16:55:56 +0900

    JS 연결 수정

--- Team2/app.js
+++ Team2/app.js
@@ -31,7 +31,7 @@ const server = http.createServer((req, res) => {
         res.end();
         return;
       }
-      res.writeHead(200, { "Content-Type": "application/script" });
+      res.writeHead(200, { "Content-Type": "application/Javascript" });
       res.end(data);
     });
   } else if (req.url === "/favicon.ico") {
