b2dfca596c97e78245ad987d24b1adf6bfbe7d3a
유으뜸 <www.goolge.com>
2024-06-12 16:31:34 +0900

    css 연결

--- Team2/app.js
+++ Team2/app.js
@@ -14,5 +14,16 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, { "Content-Type": "text/html" });
       res.end(data);
     });
+  } 
+  else if (req.url === "/style.css") {
+    fs.readFile("./public/html/style.css", "utf-8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end();
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.end(data);
+    });
   }
 });
