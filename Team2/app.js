14290073588cae877ad819e5f596340a7125f032
유으뜸 <www.goolge.com>
2024-06-12 16:51:11 +0900

    파비콘 연결

--- Team2/app.js
+++ Team2/app.js
@@ -14,8 +14,7 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, { "Content-Type": "text/html" });
       res.end(data);
     });
-  } 
-  else if (req.url === "/style.css") {
+  } else if (req.url === "/style.css") {
     fs.readFile("./public/html/style.css", "utf-8", (err, data) => {
       if (err) {
         res.writeHead(500, { "Content-Type": "text/plain" });
@@ -25,8 +24,7 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, { "Content-Type": "text/css" });
       res.end(data);
     });
-  }
-  else if(req.url === "/Script.js"){
+  } else if (req.url === "/Script.js") {
     fs.readFile("./public/html/Script.js", "utf-8", (err, data) => {
       if (err) {
         res.writeHead(500, { "Content-Type": "text/plain" });
@@ -34,6 +32,16 @@ const server = http.createServer((req, res) => {
         return;
       }
       res.writeHead(200, { "Content-Type": "application/script" });
+      res.end(data);
+    });
+  } else if (req.url === "/favicon.ico") {
+    fs.readFile("./public/html/favicon.ico", "utf-8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end();
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "img/ico" });
       res.end(data);
     });
   }
