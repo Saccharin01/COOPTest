79d207df09aec4b93309a78ac395ab478edecf17
유으뜸 <www.goolge.com>
2024-06-12 16:21:18 +0900

    html 연결

--- Team2/app.js
+++ Team2/app.js
@@ -2,3 +2,17 @@ const http = require("http");
 console.log(http);
 const fs = require("fs");
 console.log(fs);
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end();
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  }
+});
