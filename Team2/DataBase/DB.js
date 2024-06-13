a3bedc9f20edb9ae53bd9152dae587d8335fab16
Saccharin01 <chousik01@gmail.com>
2024-06-14 02:33:56 +0900

    Error Occur DB can't catch any req

--- Team2/DataBase/DB.js
+++ Team2/DataBase/DB.js
@@ -8,18 +8,23 @@ const http = require("http")
 const fs = require("fs")
 
 const server = http.createServer((req, res) => {
+  console.log(req.method)
+  console.log(req.url)
   if(req.method === "POST"){
     if(req.url === "/submit"){
-      let body = '';
       req.on("data", chunk => {
         let container = [];
       
         fs.readFile(`parseData.json`,(err,data)=>{
           if(err){ //? 파일이 없다면
             container.push(JSON.parse(chunk))
+
             console.log(container)
+
             let userId = JSON.stringify(container, null, 2) 
+
             fs.writeFile(`parseData.json`, userId, (err)=>{
+
               if(err){
                 console.log(`WF Caution : ${err}`);
               }
@@ -28,84 +33,61 @@ const server = http.createServer((req, res) => {
               }
             })
   
-          }else{ //?파일이 있다면 data
-            // console.log(JSON.parse(data))
-            // console.log(typeof(JSON.parse(data)))
-            // console.log(Array.isArray(JSON.parse(data)))
-  
-              let origindata = JSON.parse(data)
-              console.log(origindata)
-              console.log(Array.isArray(origindata))
-  
-              let userinpt = JSON.parse(chunk)
-              console.log(userinpt)
-              console.log(typeof(userinpt))
-  
-              origindata.push(userinpt)
-              console.log(origindata)
-  
-              fs.writeFile(`parseData.json`, JSON.stringify(origindata, null, 2), (err)=>{
-                if(err){
-                  console.log(err)
-                }else{
-                  console.log(`Process Tracking`)
-                }
-              })
-  
+        }else{ //?파일이 있다면 data
+          // console.log(JSON.parse(data))
+          // console.log(typeof(JSON.parse(data)))
+          // console.log(Array.isArray(JSON.parse(data)))
+
+            let origindata = JSON.parse(data)
+            console.log(origindata)
+            console.log(Array.isArray(origindata))
+
+            let userinpt = JSON.parse(chunk)
+            console.log(userinpt)
+            console.log(typeof(userinpt))
+
+            origindata.push(userinpt)
+            console.log(origindata)
+
+            fs.writeFile(`parseData.json`, JSON.stringify(origindata, null, 2), (err)=>{
+              if(err){
+                console.log(err)
+              }else{
+                console.log(`Process Tracking`)
+              }
+            })
           }
         })
       });
 
+      // req.on("end", () => {
+      //   const parseData = new URLSearchParams(body);
+      //   const id = parseData.get("user_id")
+      //   const pw = parseData.get("user_pw")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-      req.on("end", () => {
-        const parseData = new URLSearchParams(body);
-        const id = parseData.get("user_id")
-        const pw = parseData.get("user_pw")
-
-        const json = {
-          id : id,
-          pw : pw
-        };
-        const jsondata = JSON.stringify(json);
-        fs.writeFile("database.json",jsondata, (err)=>{
-          if (err) {
-            res.writeHead(500)
-            res.end('json 작성 오류');
-            return;
-          }
-          fs.readFile('./public/html/index.html', 'utf-8', (err, data)=>{
-            if(err){
-              res.writeHead(500)
-              res.end("submit후 html읽기");
-              return;
-            }else{
-              res.writeHead(200, {'content-Type': 'text/html'})
-              res.end(data);
-            }
-          });
-        })
-      })
-
+      //   const json = {
+      //     id : id,
+      //     pw : pw
+      //   };
+      //   const jsondata = JSON.stringify(json);
+      //   fs.writeFile("database.json",jsondata, (err)=>{
+      //     if (err) {
+      //       res.writeHead(500)
+      //       res.end('json 작성 오류');
+      //       return;
+      //     }
+      //     fs.readFile('./public/html/index.html', 'utf-8', (err, data)=>{
+      //       if(err){
+      //         res.writeHead(500)
+      //         res.end("submit후 html읽기");
+      //         return;
+      //       }else{
+      //         res.writeHead(200, {'content-Type': 'text/html'})
+      //         res.end(data);
+      //       }
+      //     });
+      //   })
+      // })
     }
   }
 });
