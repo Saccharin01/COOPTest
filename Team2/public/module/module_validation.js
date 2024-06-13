da46e40785ceb28df1a1e0a733fed66b7dd011a3
jojayeon <jojayeon6152@gmail.com>
2024-06-13 22:10:22 +0900

    js모듈화할 부분 복사

--- Team2/public/module/module_validation.js
+++ Team2/public/module/module_validation.js
@@ -1,4 +1,20 @@
-// 서버에 있는 것들을 모듈화하고 싶었지만 어려움이 있어 js에 있는 것을 모듈로 만들기로 했다.
+//서버에 있는 것들을 모듈화하고 싶었지만 어려움이 있어 js에 있는 것을 모듈로 만들기로 했다.
 
 
+function checkInputs() {
+  if (userId.value && userPw.value && userPwCheck.value) {
+    if (userPw.value === userPwCheck.value) {
+      submitBtn.style.display = "block";
+      document.getElementById("user_pw-check").style.borderColor = "black";
+    }
+  } else {
+    submitBtn.style.display = "none";
+    if (userPw.value !== userPwCheck.value) {
+      document.getElementById("user_pw-check").style.borderColor = "red";
+    }
+  }
+}
 
+userId.addEventListener("input", checkInputs);
+userPw.addEventListener("input", checkInputs);
+userPwCheck.addEventListener("input", checkInputs);
