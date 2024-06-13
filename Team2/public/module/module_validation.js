6c0883b581042699bf3a8f5a2562bd8f009b87fc
jojayeon <jojayeon6152@gmail.com>
2024-06-14 00:04:18 +0900

    유효성검사하는 부분 모듈화 1차 완성 테스트 전

--- Team2/public/module/module_validation.js
+++ Team2/public/module/module_validation.js
@@ -1,20 +1,37 @@
 //서버에 있는 것들을 모듈화하고 싶었지만 어려움이 있어 js에 있는 것을 모듈로 만들기로 했다.
 
+//기본 요소 준비
+const userId = document.getElementById("user_id");
+const userPw = document.getElementById("user_pw");
+const userPwCheck = document.getElementById("user_pw-check");
+const submitBtn = document.getElementById("btn");
 
-function checkInputs() {
-  if (userId.value && userPw.value && userPwCheck.value) {
-    if (userPw.value === userPwCheck.value) {
-      submitBtn.style.display = "block";
-      document.getElementById("user_pw-check").style.borderColor = "black";
-    }
-  } else {
-    submitBtn.style.display = "none";
-    if (userPw.value !== userPwCheck.value) {
-      document.getElementById("user_pw-check").style.borderColor = "red";
+export const checkInputs = () => {
+  function checkInputs2() {
+    if (userId.value && userPw.value && userPwCheck.value) {
+      if (userPw.value === userPwCheck.value) {
+        submitBtnone("block")
+        PwCheckCol("black")
+      }
+    } else {
+      submitBtnone("none")
+      if (userPw.value !== userPwCheck.value) {
+        PwCheckCol("red")
+      }
     }
   }
+  function submitBtnone(onoff) {
+    submitBtn.style.display = onoff;
+  }    
+  function PwCheckCol(col) {
+    userPwCheck.style.borderColor = col;
+  }
+
+  userId.addEventListener("input", checkInputs2); 
+  userPw.addEventListener("input", checkInputs2);
+  userPwCheck.addEventListener("input", checkInputs2);
 }
 
-userId.addEventListener("input", checkInputs);
-userPw.addEventListener("input", checkInputs);
-userPwCheck.addEventListener("input", checkInputs);
+// import checkInputs from './module/module_validation.js';
+// //호출할 때
+// checkInputs();
